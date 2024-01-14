6efc6de3fb9d9943f3e3798a7160e18bccdc5307
Josh Barker <josh@joshbarker.co.uk>
2024-01-14 15:30:43 +0000

    Init repo with base files

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+namespace Jarker\Entry;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+    }
+}
