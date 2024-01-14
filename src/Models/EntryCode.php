95dcf92536c8ba01d46d9729727561a959e62e1a
Josh Barker <josh@joshbarker.co.uk>
2024-01-14 19:25:36 +0000

    Adds has scope to EntryCode model

--- src/Models/EntryCode.php
+++ src/Models/EntryCode.php
@@ -1,9 +1,14 @@
 <?php
 namespace Jarker\Entry\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent;
 
-class EntryCode extends Model
+class EntryCode extends Eloquent\Model
 {
     public $fillable = ['code'];
+
+    public function scopeHasCode(Eloquent\Builder $query, string $code): bool
+    {
+        return $query->where('code', $code)->exists();
+    }
 }
