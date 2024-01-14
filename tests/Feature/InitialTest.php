1814fa344d56eba143e18836174fa36d56752ae4
Josh Barker <josh@joshbarker.co.uk>
2024-01-14 15:31:52 +0000

    Adds phpuni and first testt

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Feature;
+
+class InitialTest extends \Orchestra\Testbench\TestCase
+{
+    /** @test */
+    public function my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+}
